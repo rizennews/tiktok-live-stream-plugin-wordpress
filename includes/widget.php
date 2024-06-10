4e66578010b364500b918325c164813c6c362d8c
Padmore Aning <119019742+rizennews@users.noreply.github.com>
2024-06-10 01:13:51 +0000

    Update widget.php

--- includes/widget.php
+++ includes/widget.php
@@ -1,27 +1,37 @@
 <?php
-// Widget Class
-class TikTok_Live_Stream_Widget extends WP_Widget {
-    // Constructor
+class TikTokLiveStreamWidget extends WP_Widget {
     public function __construct() {
         parent::__construct(
             'tiktok_live_stream_widget',
             'TikTok Live Stream Widget',
-            array('description' => 'Display TikTok Live Stream')
+            array('description' => 'A widget to display TikTok live streams')
         );
     }
 
-    // Widget Output
     public function widget($args, $instance) {
+        $username = !empty($instance['username']) ? $instance['username'] : 'default_username';
+        $liveStreamId = getTikTokLiveStreamData($username);
+
         echo $args['before_widget'];
-        echo $args['before_title'] . 'TikTok Live Stream' . $args['after_title'];
-        echo do_shortcode('[tiktok_live_stream username="' . esc_attr($instance['username']) . '"]');
+        if (!empty($instance['title'])) {
+            echo $args['before_title'] . apply_filters('widget_title', $instance['title']) . $args['after_title'];
+        }
+        if ($liveStreamId) {
+            echo '<iframe src="https://www.tiktok.com/live/' . esc_attr($liveStreamId) . '" width="600" height="400"></iframe>';
+        } else {
+            echo handleTikTokError();
+        }
         echo $args['after_widget'];
     }
 
-    // Widget Form
     public function form($instance) {
+        $title = !empty($instance['title']) ? $instance['title'] : '';
         $username = !empty($instance['username']) ? $instance['username'] : '';
         ?>
+        <p>
+            <label for="<?php echo $this->get_field_id('title'); ?>">Title:</label>
+            <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo esc_attr($title); ?>">
+        </p>
         <p>
             <label for="<?php echo $this->get_field_id('username'); ?>">TikTok Username:</label>
             <input class="widefat" id="<?php echo $this->get_field_id('username'); ?>" name="<?php echo $this->get_field_name('username'); ?>" type="text" value="<?php echo esc_attr($username); ?>">
@@ -29,17 +39,16 @@ class TikTok_Live_Stream_Widget extends WP_Widget {
         <?php
     }
 
-    // Update Widget
     public function update($new_instance, $old_instance) {
         $instance = array();
-        $instance['username'] = (!empty($new_instance['username'])) ? sanitize_text_field($new_instance['username']) : '';
+        $instance['title'] = sanitize_text_field($new_instance['title']);
+        $instance['username'] = sanitize_text_field($new_instance['username']);
         return $instance;
     }
 }
 
-// Register Widget
 function register_tiktok_live_stream_widget() {
-    register_widget('TikTok_Live_Stream_Widget');
+    register_widget('TikTokLiveStreamWidget');
 }
 add_action('widgets_init', 'register_tiktok_live_stream_widget');
 ?>
