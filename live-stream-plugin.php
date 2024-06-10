a9033b369e34af7cf47ba91005daf00d4db1d3f7
Padmore Aning <119019742+rizennews@users.noreply.github.com>
2024-06-10 01:04:03 +0000

    Update live-stream-plugin.php

--- live-stream-plugin.php
+++ live-stream-plugin.php
@@ -3,13 +3,16 @@
 Plugin Name: TikTok Live Stream Plugin
 Plugin URI: https://github.com/rizennews/tiktok-live-stream-plugin-wordpress
 Description: Custom Plugin to embed live streams from TikTok.
-Version: 1.0
+Version: 1.0.1
 Author: Padmore Aning
 Author URI: https://github.com/rizennews
 */
 
 // Include plugin settings page
 require_once plugin_dir_path(__FILE__) . 'includes/settings-page.php';
+
+// Include additional functions
+require_once plugin_dir_path(__FILE__) . 'includes/additional-functions.php';
 
 // Include plugin functions
 require_once plugin_dir_path(__FILE__) . 'includes/functions.php';
